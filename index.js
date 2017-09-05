b7316c7f420951fb00795d4dc7cf27e5596ba5a4
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 21:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ function initForm() {
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
+function recipeInfo() {
+  var ingredientsList = document.getElementByName("ingredients")
+  var ingredients = []
+  for(var i = 0; i < ingredientsList.length; i++) {
+    if(ingredientsList[i].value !== "")
+    ingredients.push(ingredientsList[i].value)
+    }
+
+  var name = document.getElementById("name").value
+  var description = document.getElementById("description").value
+  var recipe = {name, ingredients, description}
+  return(recipe)
+  }
+
 function createRecipe() {
   var recipe = recipeInfo()
   var recipeTemplate = document.getElementById("recipe-template").innerHTML
@@ -37,21 +51,6 @@ function updateRecipe() {
 
   document.getElementById("main").innerHTML += template(recipe)
 }
-
-function recipeInfo() {
-  var ingredientsList = document.getElementByName("ingredients")
-  var ingredients = []
-  for(var i = 0; i < ingredientsList.length; i++) {
-    if(ingredientsList[i].value !== "")
-    ingredients.push(ingredientsList[i].value)
-    }
-
-  var name = document.getElementById("name").value
-  var description = document.getElementById("description").value
-  var recipe = {name, ingredients, description}
-  return(recipe)
-  }
-
 
 function handlebarsSetup() {
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
