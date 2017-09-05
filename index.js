81ac845dffd5f8f909a9f1becc326855824f5878
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 21:09:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-function handlebarsSetup() {
-  Handlebars.registerHelper('displayIngredient', function(ingredient) {
-    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
-  })
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-}
-
 function initForm() {
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
-function init() {
-  //put any page initialization/handlebars initialization here
-  handlebarsSetup()
-  initForm()
-  document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
-}
 
 
 function recipeInfo() {
@@ -68,4 +52,21 @@ function updateRecipe() {
   var template = Handlebars.compile(recipeTemplate)
 
   document.getElementById("main").innerHTML += template(recipe)
+}
+
+function handlebarsSetup() {
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
+
+function init() {
+  //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  initForm()
+  document.addEventListener("DOMContentLoaded", function(event) {
+  init()
+})
 }
