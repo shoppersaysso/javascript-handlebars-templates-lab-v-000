e8cd7d9999808ff21a6c76c716e0ea90362df37d
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 17:10:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function createRecipe() {
   var description = document.getElementById("description")
   var ingredients = document.getElementById("ingredients")
 
-  var createRecipeTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
-  var recipe = createRecipeTemplate({name: 'name', description: 'description', indgredients: 'ingredients'})
+  var recipe = formTemplate({name: 'name', description: 'description', indgredients: 'ingredients'})
 
   var submitAction = createRecipe
 
@@ -28,6 +27,7 @@ function displayEditForm() {
 }
 
 function updateRecipe() {
-
-  var submitAction = updateRecipe
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
