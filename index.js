b1538f35740b11ebfb4ae060617604c1ba8e2b4b
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 18:11:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ function init() {
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
   document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
-}
+
 
 
 function recipeInfo() {
