70fbe93dd24bbcfe476279ffe90a3f93b23adccf
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 18:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ function initForm() {
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
+function handlebarsSetup() {
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
@@ -61,12 +69,4 @@ function updateRecipe() {
   var template = Handlebars.compile(recipeTemplate)
 
   document.getElementById("main").innerHTML += template(recipe)
-}
-
-function handlebarsSetup() {
-  Handlebars.registerHelper('displayIngredient', function(ingredient) {
-    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
-  })
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 }
