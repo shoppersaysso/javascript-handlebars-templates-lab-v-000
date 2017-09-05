9ffb0a77b678da48dcc5c47bae76374366e4fca0
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 16:49:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// function init() {
+//   var recipe = {
+//     description: 'yummy chicken noodle soup',
+//     ingredients: [
+//       {quantity: "1 cup", name: 'chicken'},
+//       {quantity: "3 nanoliters", name: 'stock'},
+//       {quantity: "12", name: 'noodles'}
+//     ]
+//   }
+//
+//   var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+//   var html = template(recipe);
+// }
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   init()
+// })
+
 function createRecipe() {
 
   var description = 'yummy chicken noodle soup'
