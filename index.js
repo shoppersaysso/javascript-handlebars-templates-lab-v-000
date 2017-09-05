9b7bbdba0263a4991b98d97e7ed3dac84cd12c69
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 21:42:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
   initForm()
+}
   document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
