6c2f6e2ed6fe3937760c3b405055b50525b868a0
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 21:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // }
 
 function recipeInfo() {
-  var ingredientsList = document.getElementByName("ingredients")
+  var ingredientsList = document.getElementsByName("ingredients")
   var ingredients = []
   for(var i = 0; i < ingredientsList.length; i++) {
     if(ingredientsList[i].value !== "")
