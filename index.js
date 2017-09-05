243d43e2d6571d673a4eff49fbb03e6ef77d5ff8
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 21:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function handlebarsSetup() {
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 }
 
+function initForm() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
