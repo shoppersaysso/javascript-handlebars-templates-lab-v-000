687ce2a7dff6c1812fa2822b80bee62019711c2f
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 18:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function initForm() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
@@ -7,11 +13,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-function initForm() {
-  var formTemplate = document.getElementById("recipe-form-template").innerHTML
-  var template = Handlebars.compile(formTemplate)
-  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
-}
 
 function recipeInfo() {
   var ingredientsList = document.getElementByName("ingredients")
