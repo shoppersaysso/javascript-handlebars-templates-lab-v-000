22566d0ea2edf0373cceb33d848978735c88a231
Sara Hopper <sara.hopper@gmail.com>
2017-09-05 21:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// function initForm() {
-//   var formTemplate = document.getElementById("recipe-form-template").innerHTML
-//   var template = Handlebars.compile(formTemplate)
-//   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
-// }
+function initForm() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
 
 // function recipeInfo() {
 //   var ingredientsList = document.getElementByName("ingredients")
